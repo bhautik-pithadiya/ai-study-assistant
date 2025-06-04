3380fcf380010b45186a0fc951075b99016529e3
Bhautik Pithadiya <bhautikp@hyperlinkinfosystem.net.in>
2025-06-04 17:22:46 +0530

    Add: loading google creadentials from json file

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,6 +1,7 @@
 from pydantic_settings import BaseSettings
 from functools import lru_cache
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,6 +14,11 @@ class Settings(BaseSettings):
     # Gemini API Settings
     GEMINI_API_KEY: str = os.getenv("GEMINI_API_KEY", "")
     
+    # Google Speech-to-Text Settings
+    GOOGLE_API_CRED_PATH: str = os.getenv("GOOGLE_API_KEY", "")
+    if GOOGLE_API_CRED_PATH:
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = str(Path(GOOGLE_API_CRED_PATH).resolve())
+
     # Vector Store Settings
     VECTOR_STORE_PATH: str = "vector_store"
     
